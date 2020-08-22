34a8191a97578573c6f2dc80fa897b42d7b7631d
kevinskyba <kevinskyba@live.de>
2020-08-22 14:19:14 +0200

    updated kickbase_api to 0.0.7; updated version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Kickbase_Analysis',
-    version='0.0.1',
+    version='0.0.2',
     packages=['kickbase_analysis'],
     url='https://github.com/kevinskyba/kickbase-analysis',
     license='MIT',
